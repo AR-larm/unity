4b25e7b81fe79f9e182dc4db8b63f57c173e0573
nighttwo1 <ys12j1@gmail.com>
2020-04-18 16:01:49 +0900

    automatically create anchor on detected plane

---
+++ Assets/Scripts/Detect_Plane_script.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using GoogleARCore.Examples.Common;
+
+public class Detect_Plane_script : MonoBehaviour
+{
+    /// <summary>
+    /// A prefab for tracking and visualizing detected planes.
+    /// </summary>
+    public GameObject DetectedPlanePrefab;
+
+    /// <summary>
+    /// A list to hold new planes ARCore began tracking in the current frame. This object is
+    /// used across the application to avoid per-frame allocations.
+    /// </summary>
+    private List<DetectedPlane> m_NewPlanes = new List<DetectedPlane>();
+
+    public GameObject mun;
+
+    /// <summary>
+    /// The Unity Update method.
+    /// </summary>
+    public void Update()
+    {
+        // Check that motion tracking is tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        // Iterate over planes found in this frame and instantiate corresponding GameObjects to
+        // visualize them.
+        Session.GetTrackables<DetectedPlane>(m_NewPlanes, TrackableQueryFilter.New);
+        for (int i = 0; i < m_NewPlanes.Count; i++)
+        {
+            // Instantiate a plane visualization prefab and set it to track the new plane. The
+            // transform is set to the origin with an identity rotation since the mesh for our
+            // prefab is updated in Unity World coordinates.
+            GameObject planeObject =
+                Instantiate(DetectedPlanePrefab, Vector3.zero, Quaternion.identity, transform);
+            planeObject.GetComponent<DetectedPlaneVisualizer>().Initialize(m_NewPlanes[i]);
+
+
+
+
+            //tracking이 진행중일때 먼먼이를 plane위에 생성.
+            if (m_NewPlanes[i].TrackingState == TrackingState.Tracking)
+            {
+                GameObject obj = Instantiate(mun, new Vector3(m_NewPlanes[i].CenterPose.position.x, m_NewPlanes[i].CenterPose.position.y, m_NewPlanes[i].CenterPose.position.z), Quaternion.identity, transform);
+                obj.transform.localScale = new Vector3(1, 1, 1);
+                Anchor anchor = m_NewPlanes[i].CreateAnchor(m_NewPlanes[i].CenterPose);
+                obj.transform.parent = anchor.transform;
+                break;
+            }
+
+        }
+
+        /*
+        GameObject obj = Instantiate(mun, Vector3.zero, Quaternion.identity);
+        obj.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        obj.GetComponent<DetectedPlaneVisualizer>().Initialize(m_NewPlanes[0]);
+        */
+    }
+}
---
+++ Assets/Scripts/MunMunTouchMgr_script.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class MunMunTouchMgr_script : MonoBehaviour
+{
+    private Camera ARCamera;    //ARCore 카메라
+    public GameObject placeObject; //터치 시 평면에 생성할 프리팹
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ARCore Device 프리팹 하위에 있는 카메라를 찾아서 변수에 할당
+        ARCamera = GameObject.Find("First Person Camera").GetComponent<Camera>();
+
+        placeObject.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        //ARCore에서 제공하는 RaycastHit와 유사한 구조체
+        TrackableHit hit;
+
+        //검출 대상을 평면 또는 Feature Point로 한정
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            if ((hit.Trackable is DetectedPlane) && Vector3.Dot(ARCamera.transform.position - hit.Pose.position, hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                if (hit.Trackable is DetectedPlane)
+                {
+                    GameObject obj = Instantiate(placeObject, hit.Pose.position, hit.Pose.rotation);
+                    obj.transform.Rotate(0, 0, 0, Space.Self);
+
+                    var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                    obj.transform.parent = anchor.transform;
+                }
+            }
+        }
+
+
+
+
+
+
+
+        /*
+        if (Input.touchCount == 0)  return;
+
+        //첫 번재 터치 정보 추출
+        Touch touch = Input.GetTouch(0);
+
+        //ARCore에서 제공하는 RaycastHit와 유사한 구조체
+        TrackableHit hit;
+
+        //검출 대상을 평면 또는 Feature Point로 한정
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        //터치한 지점으로 레이 발사
+        if(touch.phase == TouchPhase.Began && Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            placeObject.transform.localScale = new Vector3(1, 1, 1);
+            //객체를 고정할 앵커를 설정
+            var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            //객체를 생성
+            GameObject obj = Instantiate(placeObject, hit.Pose.position, Quaternion.identity, anchor.transform);
+
+            //생성한 객체가 사용자 쪽을 바라보도록 회전값 계산
+            var rot = Quaternion.LookRotation(ARCamera.transform.position - hit.Pose.position);
+
+            //사용자 쪽 회전값 적용
+            obj.transform.rotation = Quaternion.Euler(ARCamera.transform.position.x, rot.eulerAngles.y, ARCamera.transform.position.z);
+        }
+        */
+
+    }
+}
