4a7cde37f562dfe4a3a4dc1f50d22e58841fab16
nighttwo1 <ys12j1@gmail.com>
2020-05-01 11:49:46 +0900

    touch manager beginning

--- Assets/Scripts/Detect_Plane_script.cs
+++ Assets/Scripts/Detect_Plane_script.cs
@@ -19,16 +19,32 @@ public class Detect_Plane_script : MonoBehaviour
 
     public GameObject mun;
 
+    //private Anchor anchor;
+    public static List<Anchor> munmuns = new List<Anchor>();    //먼먼이 anchor들을 저장. 이후 먼먼이들의 위치 및 터치시의 listener에서 사용 예정.
+
+    private Camera ARCamera;    //ARCore 카메라
+    //private int count;
+
+    public void Start()
+    {
+        ARCamera = GameObject.Find("First Person Camera").GetComponent<Camera>();
+        //count = 0;
+    }
+
     /// <summary>
     /// The Unity Update method.
     /// </summary>
     public void Update()
     {
+        Anchor anchor;
         // Check that motion tracking is tracking.
-        if (Session.Status != SessionStatus.Tracking)
+        if (Session.Status != SessionStatus.Tracking || ARCamera.transform.rotation.x<0)
         {
             return;
         }
+        //Debug.Log("camera rotation " + count + ": " + ARCamera.transform.rotation.x);
+        //count++;
+
 
         // Iterate over planes found in this frame and instantiate corresponding GameObjects to
         // visualize them.
@@ -43,19 +59,43 @@ public class Detect_Plane_script : MonoBehaviour
             planeObject.GetComponent<DetectedPlaneVisualizer>().Initialize(m_NewPlanes[i]);
 
 
-
-
             //tracking이 진행중일때 먼먼이를 plane위에 생성.
             if (m_NewPlanes[i].TrackingState == TrackingState.Tracking)
             {
                 GameObject obj = Instantiate(mun, new Vector3(m_NewPlanes[i].CenterPose.position.x, m_NewPlanes[i].CenterPose.position.y, m_NewPlanes[i].CenterPose.position.z), Quaternion.identity, transform);
+                
+                //먼먼이 object의 방향을 카메라가 보는 방향으로 설정
+                var direction = Quaternion.LookRotation(obj.gameObject.transform.position - ARCamera.transform.position).eulerAngles;
+                direction.x = m_NewPlanes[i].CenterPose.position.x;
+                direction.z = m_NewPlanes[i].CenterPose.position.z;
+                obj.transform.rotation = Quaternion.Euler(direction);
+                
+                //먼먼이 object 사이즈 변경
                 obj.transform.localScale = new Vector3(1, 1, 1);
-                Anchor anchor = m_NewPlanes[i].CreateAnchor(m_NewPlanes[i].CenterPose);
+
+                anchor = m_NewPlanes[i].CreateAnchor(m_NewPlanes[i].CenterPose);
                 obj.transform.parent = anchor.transform;
+
+                munmuns.Add(anchor);
+
+                //Debug.Log("position: " + anchor.transform.localPosition);
                 break;
             }
 
         }
+        //앵커들 위치 이동
+        /*
+        if (munmuns.Count!=0)
+        {
+            for(int i = 0; i < munmuns.Count; i++)
+            {
+                munmuns[i].transform.Translate(Vector3.forward * Time.deltaTime * 0.5f);
+                //Debug.Log("position of MunMun Num "+i+": " + munmuns[i].transform.localPosition);
+
+            }
+        }
+        */
+        
 
         /*
         GameObject obj = Instantiate(mun, Vector3.zero, Quaternion.identity);
--- Assets/Scripts/MunMunTouchMgr_script.cs
+++ Assets/Scripts/MunMunTouchMgr_script.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleARCore;
+using System.IO;
 
 public class MunMunTouchMgr_script : MonoBehaviour
 {
@@ -20,6 +21,14 @@ public class MunMunTouchMgr_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Camera Position: " + ARCamera.transform.position);
+        if (Detect_Plane_script.munmuns.Count != 0)
+        {
+            for (int i = 0; i < Detect_Plane_script.munmuns.Count; i++)
+            {
+                Debug.Log("Position of MunMun Num " + i + ": " + Detect_Plane_script.munmuns[i].transform.localPosition);
+                }
+        }
         Touch touch;
         if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
         {
@@ -42,12 +51,24 @@ public class MunMunTouchMgr_script : MonoBehaviour
             {
                 if (hit.Trackable is DetectedPlane)
                 {
-                    GameObject obj = Instantiate(placeObject, hit.Pose.position, hit.Pose.rotation);
-                    obj.transform.Rotate(0, 0, 0, Space.Self);
+                    Debug.Log("Hit Position: " + hit.Pose.position);
+                    for (int i=0;i<Detect_Plane_script.munmuns.Count; i++)
+                    {
 
-                    var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                        //hit position과 먼먼이 position 비교. 오차 범위 내에 먼먼이 위치시에 동작.
+                        if((hit.Pose.position.x-0.03f<=Detect_Plane_script.munmuns[i].transform.position.x &&hit.Pose.position.x+0.02f>Detect_Plane_script.munmuns[i].transform.position.x)
+                            && (hit.Pose.position.y - 0.03f < Detect_Plane_script.munmuns[i].transform.position.y && hit.Pose.position.y + 0.07f > Detect_Plane_script.munmuns[i].transform.position.y)
+                            && (hit.Pose.position.z - 0.1f < Detect_Plane_script.munmuns[i].transform.position.z && hit.Pose.position.z + 0.02f > Detect_Plane_script.munmuns[i].transform.position.z))
+                        {
+                            GameObject obj = Instantiate(placeObject, hit.Pose.position, hit.Pose.rotation);
+                            obj.transform.Rotate(0, 0, 0, Space.Self);
 
-                    obj.transform.parent = anchor.transform;
+                            var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                            obj.transform.parent = anchor.transform;
+                        }
+                    }
+                    
                 }
             }
         }
